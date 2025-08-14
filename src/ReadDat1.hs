f9ad2f0f9ff9541cc64e0229557cacc4d9eb7cc5
CaryRock <carytherock@gmail.com>
2025-08-14 16:09:22 -0400

    Some packages for the package.yaml, trying to figure how to read a stupid, simple json file and get the values out of it into the program

--- src/ReadDat1.hs
+++ src/ReadDat1.hs
@@ -1,8 +1,17 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module ReadDat1 (readDat1) where
 
+import GHC.Generics
 import Data.Aeson
+import Data.Text
+import qualified Data.Aeson.Types as A
 import System.Directory (getCurrentDirectory)
 import qualified Data.ByteString.Lazy as B
+
+newtype Content = Content
+  { contents :: String } deriving (Show, Generic)
 
 readDat1 :: IO ()
 readDat1 = do
@@ -10,4 +19,7 @@ readDat1 = do
   let
     dataDir = curDir ++ "/data"
   testDat <- B.readFile (dataDir ++ "/testDat1.json")
-  print testDat
+  case eitherDecode testDat :: Either String Value of
+    Left  _ -> putStrLn "Decode failed"
+    Right x -> print x
+  --print testDat
